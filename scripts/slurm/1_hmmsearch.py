450540505a883c37f8cb150b024aa654a4b7cca1
amsesk <amsesk@gl-login1.arc-ts.umich.edu>
2020-12-29 12:30:43 -0500

    some QOL edits

--- scripts/slurm/1_hmmsearch.py
+++ scripts/slurm/1_hmmsearch.py
@@ -7,8 +7,9 @@ import os
 NJOBS=10
 PEP_PATH = sys.argv[1]
 MARKERS_PATH = sys.argv[2]
+SUFFIX = ".faa"
 
-todo = [x for x in os.listdir(PEP_PATH) if x.endswith(".aa.fasta")]
+todo = [os.path.join(PEP_PATH,x) for x in os.listdir(PEP_PATH) if x.endswith(SUFFIX)]
 todo = np.array(todo)
 todo_spl = np.array_split(todo, NJOBS)
 
@@ -22,6 +23,7 @@ for idx,chunk in enumerate(todo_spl):
             time = 48
             )
     for line in chunk:
-        sg.add_command(f"hmmsearch --cpu 1 -E 1e-5 --domtblout {line.replace('.aa.fasta', 'odb10.domtbl')} {MARKERS_PATH} {line} > {line+'.hmmsearch.log'}")
+        outline = os.path.basename(line)
+        sg.add_command(f"hmmsearch --cpu 1 -E 1e-5 --domtblout {outline.replace(SUFFIX, 'odb10.domtbl')} {MARKERS_PATH} {line} > {outline+'.hmmsearch.log'}")
 
     sg.write()
