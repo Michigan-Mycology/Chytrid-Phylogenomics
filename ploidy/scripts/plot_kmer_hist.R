eebdeabe492850140feec141f75364202d679aad
amsesk <amsesk@umich.edu>
2021-02-02 10:56:49 -0500

    generalize xlim, I think?

--- ploidy/scripts/plot_kmer_hist.R
+++ ploidy/scripts/plot_kmer_hist.R
@@ -6,12 +6,14 @@ KmerHistGen <- function(prefix, dirpath, coverages = NULL) {
     median_coverage = round(coverages[coverages$strain == basename(prefix),]$median_coverage, 2)
   }
   hist = read.table(file.path(dirpath, paste(prefix, ".khist", sep="")), col.names = c("depth", "rawcount", "count"), sep="\t")
-  peaks = read.table(file.path(dirpath, paste(prefix, ".peaks", sep="")), comment.char = "#", col.names=c("start","center","stop","max","volume"), sep="\t")
+  peaks = read.table(file.path(dirpath, paste(prefix, ".peaks", sep="")), comment.char = "#", col.names=c("start","center","stop","max","volume"), sep="\t") %>%
+    arrange(desc(volume))
+  #print(peaks)
   ggplot(hist) +
     geom_point(aes(x=depth, y=count)) +
     geom_line(aes(x=depth, y=count)) +
     ylim(0,max(hist$count)*1.25) +
-    xlim(0,min(peaks$stop)*3.00) +
+    xlim(0,peaks[1,]$stop*3.0) +
     geom_vline(xintercept = peaks[1,]$center, colour = "blue", alpha=0.5) +
     geom_vline(xintercept = peaks[2,]$center, colour = "blue", alpha=0.5) +
     #annotate("text", x=Inf, y=Inf, label=paste("mean coverage = ", mean_coverage, "x", sep=""), vjust=1.2, hjust=1.2, cex=5) +
@@ -22,6 +24,7 @@ KmerHistGen <- function(prefix, dirpath, coverages = NULL) {
 args = commandArgs(trailingOnly = TRUE)
 
 files = list.files(args[1])
+
 
 # Get prefixes only
 prefixes = c()
