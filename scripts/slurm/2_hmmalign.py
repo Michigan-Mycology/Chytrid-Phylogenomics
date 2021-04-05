fc7ecef21f70dd54c67172dc820183c7ac7b8659
amsesk <amsesk@umich.edu>
2021-04-05 18:02:39 -0400

    make all these use the CHTYRID_PHYLO

--- scripts/slurm/2_hmmalign.py
+++ scripts/slurm/2_hmmalign.py
@@ -1,10 +1,11 @@
 import sys
-sys.path.append("/home/amsesk/scripts/")
+sys.path.append(os.path.join(os.environ.get(
+    'CHYTRID_PHYLO'), "scripts", "slurm"))
 import scriptgen
 import numpy as np
 import os
 
-NJOBS=10
+NJOBS = 10
 UNALN_PATH = os.path.abspath(sys.argv[1])
 MARKERS_PATH = os.path.abspath(sys.argv[2])
 
@@ -14,19 +15,22 @@ todo_spl = np.array_split(todo, NJOBS)
 
 jobname = "HmAl"
 
-for idx,chunk in enumerate(todo_spl):
+for idx, chunk in enumerate(todo_spl):
     sg = scriptgen.SlurmScriptGenerator(
-            jobname = f"{jobname}_{idx}",
-            cpus_per_task = 1,
-            mem_per_cpu = 2,
-            time = 48
-            )
+        jobname=f"{jobname}_{idx}",
+        cpus_per_task=1,
+        mem_per_cpu=2,
+        time=48
+    )
     for line in chunk:
         sg.add_command(f"hmmalign --trim --amino -o {line.replace('.fasta','.aa.msa')} {os.path.join(MARKERS_PATH, line.replace('.fasta','.hmm'))} {line}")
         sg.add_command(f"esl-reformat --replace=x:- --gapsym=- -o {line.replace('.fasta','.esltmp')} afa {line.replace('.fasta','.aa.msa')}")
-        sg.add_command("perl -p -e 'if (! /^>/) {{ s/[ZBzbXx\*]/-/g }}' {} > {}".format(line.replace('.fasta','.esltmp'), line.replace('.fasta','.clnaln')))
-        #sg.add_command(f"rm {line.replace('.fasta','.esltmp')}")
-        #sg.add_command(f"trimal -automated1 -in {line.replace('.fasta','.clnaln')} -out {line.replace('.fasta','.aa.filter')}")
+        sg.add_command("perl -p -e 'if (! /^>/) {{ s/[ZBzbXx\*]/-/g }}' {} > {}".format(
+            line.replace('.fasta', '.esltmp'), line.replace('.fasta', '.clnaln')))
+        # sg.add_command(f"rm {line.replace('.fasta','.esltmp')}")
+        # sg.add_command(f"trimal -automated1 -in
+        # {line.replace('.fasta','.clnaln')} -out
+        # {line.replace('.fasta','.aa.filter')}")
         sg.add_command(f"trimal -resoverlap 0.50 -seqoverlap 60 -in {line.replace('.fasta','.clnaln')} -out {line.replace('.fasta','.aa.filter')}")
         sg.add_command(f"trimal -automated1 -fasta -in {line.replace('.fasta','.aa.filter')} -out {line.replace('.fasta','.aa.trim')}")
         sg.add_command(f"python $CHYTRID_PHYLO/scripts/python/remove_gapped_seqs.py {line.replace('.fasta', '.aa.trim')} > {line.replace('.fasta', '.aa.trim.rmgapped')}")
--- scripts/slurm/3_genetrees.py
+++ scripts/slurm/3_genetrees.py
@@ -1,10 +1,11 @@
 import sys
-sys.path.append("/home/amsesk/scripts/")
+sys.path.append(os.path.join(os.environ.get(
+    'CHYTRID_PHYLO'), "scripts", "slurm"))
 import scriptgen
 import numpy as np
 import os
 
-NJOBS=20
+NJOBS = 20
 ALN_PATH = os.path.abspath(sys.argv[1])
 
 todo = [f"{os.path.join(ALN_PATH,x)}" for x in os.listdir(ALN_PATH) if x.endswith(".aa.trim.rmgapped")]
@@ -13,13 +14,13 @@ todo_spl = np.array_split(todo, NJOBS)
 
 jobname = "FaTr"
 
-for idx,chunk in enumerate(todo_spl):
+for idx, chunk in enumerate(todo_spl):
     sg = scriptgen.SlurmScriptGenerator(
-            jobname = f"{jobname}_{idx}",
-            cpus_per_task = 1,
-            mem_per_cpu = 2,
-            time = 1
-            )
+        jobname=f"{jobname}_{idx}",
+        cpus_per_task=1,
+        mem_per_cpu=2,
+        time=1
+    )
     sg.add_command("module load singularity")
     sg.add_command("module load fasttree")
     for line in chunk:
