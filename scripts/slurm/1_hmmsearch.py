c9417adeed7355d4082929da8ac093aef1e67a69
amsesk <amsesk@gl-login1.arc-ts.umich.edu>
2021-04-05 16:09:30 -0400

    040521_update_gus

--- scripts/slurm/1_hmmsearch.py
+++ scripts/slurm/1_hmmsearch.py
@@ -1,15 +1,17 @@
 import sys
+import os
 sys.path.append(os.path.join(os.environ.get('CHYTRID_PHYLO'), "scripts", "slurm"))
 import scriptgen
 import numpy as np
-import os
+import re
 
 NJOBS=10
 PEP_PATH = sys.argv[1]
 MARKERS_PATH = sys.argv[2]
-SUFFIX = ".faa"
+SUFFIX = "([.]fa$|[.]fasta$)"
+SUFFIX_PATTERN = re.compile(SUFFIX)
 
-todo = [os.path.join(PEP_PATH,x) for x in os.listdir(PEP_PATH) if x.endswith(SUFFIX)]
+todo = [os.path.join(PEP_PATH,x) for x in os.listdir(PEP_PATH) if re.search(SUFFIX_PATTERN, x) is not None]
 todo = np.array(todo)
 todo_spl = np.array_split(todo, NJOBS)
 
@@ -24,6 +26,6 @@ for idx,chunk in enumerate(todo_spl):
             )
     for line in chunk:
         outline = os.path.basename(line)
-        sg.add_command(f"hmmsearch --cpu 1 -E 1e-5 --domtblout {outline.replace(SUFFIX, 'odb10.domtbl')} {MARKERS_PATH} {line} > {outline+'.hmmsearch.log'}")
+        sg.add_command(f"hmmsearch --cpu 1 -E 1e-5 --domtblout {re.sub(SUFFIX_PATTERN, '.odb10.domtbl', outline)} {MARKERS_PATH} {line} > {outline+'.hmmsearch.log'}")
 
     sg.write()
