94f04d0e038bf11c56f4c6b249834937ed53aa3f
amsesk <amsesk@umich.edu>
2022-02-03 22:47:25 -0800

    initial

---
+++ scripts/R/batch_rename_tips.R
@@ -0,0 +1,32 @@
+library(phytools)
+library(tidyverse)
+library(readxl)
+
+nametips = function(lab, isolates) {
+  spl = strsplit(lab, split="[|]")
+  new = isolates %>% 
+    filter(LTP == spl[[1]][1]) %>%
+    select (SPECIES.TREE.LABEL) %>%
+    .$SPECIES.TREE.LABEL
+  paste(new, lab, sep="&")
+}
+
+args = commandArgs(trailingOnly = TRUE)
+tree_path = args[1]
+isolates_path = args[2]
+
+setwd(tree_path)
+isolates = read_xlsx(isolates_path) %>%
+  select(SPECIES.TREE.LABEL, LTP)
+
+files = list.files(path = tree_path)
+markers = lapply(files, (function(f) strsplit(f, split="[.]")[[1]][1]) )
+
+flist = as.list(files)
+names(flist) = unlist(markers)
+
+for (f in flist) {
+  t = read.newick(f)
+  t$tip.label = sapply(as.list(t$tip.label), nametips, isolates = isolates)
+  write.tree(t, file = gsub(".aa.tre", ".aa.tre.renamed", f))
+}
